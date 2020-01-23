c5ff7e42a0e52855764e0d263f70db64c6fc04aa
Devin Shoemaker <devinshoe@gmail.com>
2020-01-22 20:09:39 -0600

    starters/blank: configure service worker

--- apps/starters/blank/src/main.tsx
+++ apps/starters/blank/src/main.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './app/app';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -11,3 +10,8 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
