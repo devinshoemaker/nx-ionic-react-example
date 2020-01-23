073b7599beedd5e3d883e45dacf336dfb19ba04e
Devin Shoemaker <devinshoe@gmail.com>
2020-01-22 21:01:49 -0600

    starter/blank: generate home page component
    `nx g @nrwl/react:c home --directory pages/home --project starters-blank`

---
+++ apps/starters/blank/src/app/pages/home/home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './home.css';
+
+/* eslint-disable-next-line */
+export interface HomeProps {}
+
+export const Home = (props: HomeProps) => {
+  return (
+    <div>
+      <h1>Welcome to home component!</h1>
+    </div>
+  );
+};
+
+export default Home;
