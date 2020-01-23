1b082d6e53da626eb95c16342bc6700859f1ce7c
Devin Shoemaker <devinshoe@gmail.com>
2020-01-22 20:13:21 -0600

    starters/blank: add Ionic blank starter template

--- apps/starters/blank/src/app/app.spec.tsx
+++ apps/starters/blank/src/app/app.spec.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-
-import { BrowserRouter } from 'react-router-dom';
-
 import App from './app';
 
-describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-
-    expect(baseElement).toBeTruthy();
-  });
-
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-
-    expect(getByText('Welcome to blank!')).toBeTruthy();
-  });
+test('renders without crashing', () => {
+  const { baseElement } = render(<App />);
+  expect(baseElement).toBeDefined();
 });
